bd3c2d5448e10fda01154e5015a1671d7e56fa05
limroof <ismail.elhadaj@gmail.com>
2022-12-03 04:17:18 -0500

    setup gh-pages

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -20,6 +20,7 @@ export default function Home() {
         <SectionFive></SectionFive>
         <SectionSix></SectionSix>
         <SectionSev></SectionSev>
+        <Footer></Footer>
       </Main>
     </React.Fragment>
   );
