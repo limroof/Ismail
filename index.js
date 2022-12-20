c1c583c7f2355b913a998567de7b6ed9f5303557
limroof <ismail.elhadaj@gmail.com>
2022-12-19 22:49:08 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT;
 const userRoute = require("./Routes/users");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
 const USER = process.env.USERNAME;
-const PSW = process.env.PASSWORD ;
+const PSW = process.env.PASSWORD;
 
- module.export = mongoose
+module.export = mongoose
   .connect(
     `mongodb+srv://${USER}:${PSW}@cluster0.zx9nhb9.mongodb.net/?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
-/* 
-const mongoDB = "mongodb://127.0.0.1:27017/UsersTodos";
-mongoose.connect(mongoDB, { useNewUrlParser: true });
-const db = mongoose.connection; */
 
+/* const mongoDB = "mongodb://127.0.0.1:27017/UsersTodos";
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+const db = mongoose.connection;
+ */
 app.use(
   cors({
     origin: "https://localhost:3000",
@@ -29,9 +29,6 @@ app.use(
 );
 
 app.use("/users/", userRoute);
-const customerRoute = require("./Routes/customers");
-
-app.use("/customers", customerRoute);
 
 app.listen(port, () => {
   console.log("Example app listening on port ", port);
