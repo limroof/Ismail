752ad34538089db19deddb58bc6dc08b1add649a
limroof <ismail.elhadaj@gmail.com>
2022-12-20 01:15:11 -0500

    initial commit

--- Routes/users.js
+++ Routes/users.js
@@ -10,10 +10,6 @@ var cookieParser = require("cookie-parser");
 
 userRouter.use(bodyParser.json());
 userRouter.use(cookieParser());
-userRouter.setHeader(
-  "Access-Control-Allow-Origin",
-  "https://limroof.github.io/"
-);
 
 // ******************** USER ROUTE ******************************
 userRouter.post("/register", userCtrl.signup);
